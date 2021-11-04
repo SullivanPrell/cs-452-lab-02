4e6e5b6350007000a5b9f03b7dcd93c19fff3571
Sullivan Prellwitz <sullivan.prell@gmail.com>
2021-11-04 10:47:01 -0500

    Initialize Repo

---
+++ cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/sulli/OneDrive - UW-Eau Claire/Computer Science/cs 452/cs-452-lab-02/main.cpp" "C:/Users/sulli/OneDrive - UW-Eau Claire/Computer Science/cs 452/cs-452-lab-02/cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs_452_lab_02.dir/main.cpp.obj"
+  "cs_452_lab_02.exe"
+  "cs_452_lab_02.exe.manifest"
+  "cs_452_lab_02.lib"
+  "cs_452_lab_02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs_452_lab_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

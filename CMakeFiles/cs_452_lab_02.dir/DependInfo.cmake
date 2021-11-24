81248f6965fb70b7aebb82a39578afad50ef42f6
Sullivan Prellwitz <prellwsr8859@uwec.edu>
2021-11-23 22:20:00 -0600

    Update makefile

---
+++ CMakeFiles/cs_452_lab_02.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/Queue.cpp" "CMakeFiles/cs_452_lab_02.dir/Queue.cpp.o" "gcc" "CMakeFiles/cs_452_lab_02.dir/Queue.cpp.o.d"
+  "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/main.cpp" "CMakeFiles/cs_452_lab_02.dir/main.cpp.o" "gcc" "CMakeFiles/cs_452_lab_02.dir/main.cpp.o.d"
+  "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/mfqs.cpp" "CMakeFiles/cs_452_lab_02.dir/mfqs.cpp.o" "gcc" "CMakeFiles/cs_452_lab_02.dir/mfqs.cpp.o.d"
+  "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/process.cpp" "CMakeFiles/cs_452_lab_02.dir/process.cpp.o" "gcc" "CMakeFiles/cs_452_lab_02.dir/process.cpp.o.d"
+  "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/rt.cpp" "CMakeFiles/cs_452_lab_02.dir/rt.cpp.o" "gcc" "CMakeFiles/cs_452_lab_02.dir/rt.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/DependInfo.cmake
@@ -1,18 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/sulli/OneDrive - UW-Eau Claire/Computer Science/cs 452/cs-452-lab-02/Queue.cpp" "C:/Users/sulli/OneDrive - UW-Eau Claire/Computer Science/cs 452/cs-452-lab-02/cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/Queue.cpp.obj"
-  "C:/Users/sulli/OneDrive - UW-Eau Claire/Computer Science/cs 452/cs-452-lab-02/main.cpp" "C:/Users/sulli/OneDrive - UW-Eau Claire/Computer Science/cs 452/cs-452-lab-02/cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/main.cpp.obj"
-  "C:/Users/sulli/OneDrive - UW-Eau Claire/Computer Science/cs 452/cs-452-lab-02/process.cpp" "C:/Users/sulli/OneDrive - UW-Eau Claire/Computer Science/cs 452/cs-452-lab-02/cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/process.cpp.obj"
-  "C:/Users/sulli/OneDrive - UW-Eau Claire/Computer Science/cs 452/cs-452-lab-02/rt.cpp" "C:/Users/sulli/OneDrive - UW-Eau Claire/Computer Science/cs 452/cs-452-lab-02/cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/rt.cpp.obj"
+  "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/Queue.cpp" "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/Queue.cpp.o"
+  "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/main.cpp" "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/main.cpp.o"
+  "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/mfqs.cpp" "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/mfqs.cpp.o"
+  "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/process.cpp" "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/process.cpp.o"
+  "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/rt.cpp" "/home/sullivan/Documents/compsci/cs452/cs-452-lab-02/cmake-build-debug/CMakeFiles/cs_452_lab_02.dir/rt.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "MSVC")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
